6bdc5a162971739860d901eb8285aaeb2fa7bd1d
Pablo Tesone <tesonep@gmail.com>
2017-04-25 16:16:58 +0200

    Fixing problem with loading epicea while running

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -3,10 +3,15 @@
 set -ex
 
 ./pharo Pharo.image eval "
+
+EpMonitor current disable.
+
 Metacello new 
 	baseline: 'AtomicClassInstaller';
 	repository: 'filetree://./mc';
 	load.
+
+EpMonitor current enable.
 
 Smalltalk saveAs:'atomicPharoInstaller'.
 "
